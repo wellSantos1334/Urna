1c4630daea98eee0c2922f9498fd092a677e2fc2
well <44660239+wellzin134@users.noreply.github.com>
2022-04-19 22:39:17 -0300

    project 1.0

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '11111',
+                nome: 'Bolsomituh',
+                partido: 'MITOMITO',
+                fotos: [
+                    { url: 'vereador1.jfif', legenda: 'Vereador', small: false }
+                ]
+            },
+            {
+                numero: '22222',
+                nome: 'TemerzinOBRABO',
+                partido: 'SoOsBRABOS',
+                fotos: [
+                    { url: 'vereador2.jpg', legenda: 'Vereador', small: false }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '33',
+                nome: 'Lulinha',
+                partido: 'PTRALHA',
+                vice: 'Dilminha',
+                fotos: [
+                    { url: 'prefeito1.jfif', legenda: 'Prefeito' },
+                    { url: 'viceprefeito1.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: '44',
+                nome: 'Daciolo',
+                partido: 'SóOsDoidos',
+                vice: 'CiraoDaMassa',
+                fotos: [
+                    { url: 'prefeito2.jpg', legenda: 'Prefeito' },
+                    { url: 'viceprefeito2.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    }
+];
